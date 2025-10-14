9f3451622ad9c449e9153d7e590505b530da3542
Stevie Gillen <stevie_gillen@hotmail.com>
2025-10-14 22:35:15 +0100

    Complete basic game logic - Needs refactored

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const CreateBoard = (() => {
+// set up game board
+const Board = (() => {
 	// initial settings
 	const board = [];
 	const rows = 3;
@@ -19,21 +20,71 @@ const CreateBoard = (() => {
 	board.forEach((tile) => {
 		gameBoard.appendChild(tile);
 	});
+
+	return { board };
 })();
 
-const CreatePlayer = (() => {
+// set up player functionality
+const Player = (() => {
 	let activePlayer = 0;
-	const markers = ['X', 'O'];
+	const playerOnePositions = [];
+	const playerTwoPositions = [];
+	const positions = [playerOnePositions, playerTwoPositions];
 	// set active player
 	let currentPlayer = document.querySelector(`.player--${activePlayer}`);
 	currentPlayer.classList.add('active-player');
 	// switch players
 	const switchPlayers = () => {
 		currentPlayer.classList.toggle('active-player');
-		activePlayer = activePlayer == 1 ? 0 : 1;
+		activePlayer = activePlayer == 0 ? 1 : 0;
 		currentPlayer = document.querySelector(`.player--${activePlayer}`);
 		currentPlayer.classList.toggle('active-player');
+		return activePlayer;
 	};
 
-	return { switchPlayers };
+	return { activePlayer, positions, switchPlayers };
+})();
+
+// play game
+const playGame = (() => {
+	// store game state
+	let isGameOver = false;
+	// store player index
+	let playerIndex = Player.activePlayer;
+	// store active marker
+	const Markers = ['X', 'O'];
+	let currentMarker = Markers[Player.activePlayer];
+	// winning patterns
+	const winningPatterns = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	// add playermarker to the board and switch player
+	Board.board.forEach((tile) => {
+		tile.addEventListener('click', () => {
+			if (!tile.innerText) {
+				tile.innerText = currentMarker;
+				Player.positions[playerIndex].push(Board.board.indexOf(tile));
+				// check for winning combination
+				winningPatterns.forEach((card) => {
+					let result = card.every((num) => Player.positions[playerIndex].includes(num));
+					if (result) {
+						console.log('WINNER 🎊');
+						isGameOver = true;
+					}
+				});
+				if (!isGameOver) {
+					console.log(Player.positions[playerIndex]);
+					playerIndex = Player.switchPlayers();
+					currentMarker = Markers[playerIndex];
+				}
+			}
+		});
+	});
 })();
