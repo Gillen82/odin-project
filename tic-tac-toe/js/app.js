603a12f7becc70dcd2c60d6fb6cdfb09b07ec202
Stevie Gillen <stevie_gillen@hotmail.com>
2025-10-14 20:31:14 +0100

    Set up players

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const CreateBoard = (() => {
-	// get game board element
-	const gameBoard = document.querySelector('.game-board');
 	// initial settings
 	const board = [];
 	const rows = 3;
 	const cols = 3;
+	// get game board element
+	const gameBoard = document.querySelector('.game-board');
 	// create tile elements
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < cols; j++) {
@@ -19,4 +19,21 @@ const CreateBoard = (() => {
 	board.forEach((tile) => {
 		gameBoard.appendChild(tile);
 	});
+})();
+
+const CreatePlayer = (() => {
+	let activePlayer = 0;
+	const markers = ['X', 'O'];
+	// set active player
+	let currentPlayer = document.querySelector(`.player--${activePlayer}`);
+	currentPlayer.classList.add('active-player');
+	// switch players
+	const switchPlayers = () => {
+		currentPlayer.classList.toggle('active-player');
+		activePlayer = activePlayer == 1 ? 0 : 1;
+		currentPlayer = document.querySelector(`.player--${activePlayer}`);
+		currentPlayer.classList.toggle('active-player');
+	};
+
+	return { switchPlayers };
 })();
